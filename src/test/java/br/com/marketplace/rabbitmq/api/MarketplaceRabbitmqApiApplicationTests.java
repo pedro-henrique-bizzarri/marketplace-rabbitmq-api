ec898783fbd1e09539dd1bb0912358b53865e3ea
Pedro Henrique Bizzarri <pedrobizzarri@gmail.com>
2025-01-18 03:54:23 -0300

    Configurando projeto

---
+++ src/test/java/br/com/marketplace/rabbitmq/api/MarketplaceRabbitmqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marketplace.rabbitmq.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceRabbitmqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
