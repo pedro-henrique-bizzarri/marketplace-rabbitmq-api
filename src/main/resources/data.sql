afb476a1649abb3719847817d3c262b3799a6463
Pedro Henrique Bizzarri <pedrobizzarri@gmail.com>
2025-01-20 12:40:15 -0300

    Criando carga inicial do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_usuario (cod_usuario, nome, cpf, data_criacao, data_ultima_atualizacao) VALUES (NEXTVAL('sq_usuario'), 'Stella Marli Hadassa Silva', '042.105.641-09', '2025-01-18 13:55:00', '2025-01-18 13:55:00');
+
+INSERT INTO tb_usuario (cod_usuario, nome, cpf, data_criacao, data_ultima_atualizacao) VALUES (NEXTVAL('sq_usuario'), 'Renan Márcio Nelson Melo', '724.063.667-40', '2025-01-18 14:10:00', '2025-01-18 14:10:00');
+
+INSERT INTO tb_produto (cod_produto, nome, descricao, preco, estoque) VALUES (NEXTVAL('sq_produto'), 'Tenis', 'Tênis leve e confortável, com solado antiderrapante e tecido respirável, perfeito para o dia a dia.', 1099.90, 10);
+
+INSERT INTO tb_produto (cod_produto, nome, descricao, preco, estoque) VALUES (NEXTVAL('sq_produto'), 'Calça Jeans', 'Calça jeans clássica, confortável e versátil, ideal para qualquer ocasião.', 579.90, 20);
+
+INSERT INTO tb_produto (cod_produto, nome, descricao, preco, estoque) VALUES (NEXTVAL('sq_produto'), 'Camiseta', 'Camiseta básica, leve e confortável, perfeita para o dia a dia.', 79.90, 120);
+
+INSERT INTO tb_produto (cod_produto, nome, descricao, preco, estoque) VALUES (NEXTVAL('sq_produto'), 'Óculos de sol', 'Óculos de sol estiloso com proteção UV, ideal para qualquer ocasião.', 349.90, 15);
+
